a021519c8bf0fbe79a9bf59e9edc31517905e468
Carlos Medina <spark_343@hotmail.com>
2022-07-25 01:18:44 -0500

    Se agrega ejercicio 1

---
+++ src/ejercicios/readme/Ej1.scala
@@ -0,0 +1,47 @@
+package ejercicios.readme
+
+import scala.:+
+
+/*
+###Ejercicio 1:
+
+Cierta persona encargada del cifrado de caracteres requiere de un programa que le permita pasar una vocal a otro
+carácter, las correspondencias son las siguientes:
+
+a = L
+
+e = 9
+
+i = $
+
+o = R
+
+u = 4
+
+Ejercicio: Implementa la lógica correspondiente para que la persona pueda terminar su día laboral.
+
+def encryptVowel(vowelsList: List[Char]): List[Char] = ???
+
+NOTA: La persona tiene estrictamente prohibido realizar la correspondencia a través de sentencias match, case o if
+puesto que estaría violando su contrato. No hagas que pierda su empleo :(
+
+Ejemplo de entrada y salida:
+
+encryptVowel(List('a', 'i', 'e', 'a')) => List(L, $, 9, L)
+encryptVowel(List('a')) => List(L)
+encryptVowel(List('i', 'e')) => List($, 9)
+ */
+object Ej1 extends App{
+  def encryptVowel(vowelsList: List[Char]): List[Char] = {
+    val encrypt = Map('a' -> 'L', 'e' -> '9', 'i' -> '$', 'o' -> 'R', 'u' -> '4')
+    var res:List[Char] = List()
+    for (letter <- vowelsList) {
+      //println(encrypt(letter))
+      res = res :+ encrypt(letter)
+    }
+    res
+  }
+  println(encryptVowel(List('a', 'i', 'e', 'a')))
+  println(encryptVowel(List('a')))
+  println(encryptVowel(List('i', 'e')))
+}
